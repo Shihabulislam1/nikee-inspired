ce8df2d6669217cd78baa9ea24c2fa1af403dfbd
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2024-04-17 13:01:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <p className='bg-blue-200 text-slate-600 bg-gradient-to-r from-blue-200 to-green-400 py-5 rounded-3xl shadow-lg text-clip text-center flex flex-col justify-center h-full w-full hover:animate-pulse'>Heloo</p>
+    </>
+  )
+}
+
+export default App
