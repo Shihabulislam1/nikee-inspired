8f7b87d1b69c70bd5a968ee80d28f35ea8878496
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2024-04-17 14:15:02 +0600

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-
-import './App.css'
+import "./App.css";
+import CustomerReviews from "./sections/CustomerReview";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProduct from "./sections/PopularProduct";
+import Services from "./sections/Services";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
 
 function App() {
- 
   return (
-    <>
-      <p className='bg-blue-200 text-slate-600 bg-gradient-to-r from-blue-200 to-green-400 py-5 rounded-3xl shadow-lg text-clip text-center flex flex-col justify-center h-full w-full hover:animate-pulse'>Heloo</p>
-    </>
-  )
+    <main className="realtive">
+      nav
+      <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+      <section className="padding"><PopularProduct/></section>
+      <section className="padding"><SuperQuality/></section>
+      <section className="padding-x py-10"><Services/></section>
+      <section className="padding bg-pale-blue"><CustomerReviews/></section>
+      <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/></section>
+      <section className="bg-black padding-x padding-t pb-8"><Footer/></section>
+    </main>
+  );
 }
 
-export default App
+export default App;
